8502eef68b144d19975d99b960599f6f24843033
vincentlee <axwack@gmail.com>
2016-01-04 22:57:33 -0500

    Another milestone. Can save a user defined tree in Adjacency List model.

---
+++ equity/utils/JSONTree.py
@@ -0,0 +1,69 @@
+import json
+from equity.models import UserSecuritySelectionModel, SecuritySelectionModels, ClassificationNames
+
+get = lambda node_id: UserSecuritySelectionModel.objects.get(pk=node_id)
+
+
+def JSONtoTreeBeard(obj):
+    id = None;
+
+    if (isinstance(obj, dict)) == True:
+        treeString = obj
+    else:
+        treeString = json.loads(obj)
+
+        # hierarchy = dict(treeString)
+
+        root = SecuritySelectionModels.objects.filter(pk=treeString[0]['id'])
+
+        if root:
+            rootNode = UserSecuritySelectionModel()
+            rootNode.currWeight = 0
+
+            rootNode.SSM = SecuritySelectionModels.objects.get(pk=treeString[0]['id'])
+            id = treeString[0]['id']
+
+            rootNode.classificationNameNode = None
+            rootNode.ext_model_id = treeString[0]['id']
+            rootNode.hasChildnode = treeString[0]['hasChildNode']
+            rootNode.tgtWeight = 0
+            root = UserSecuritySelectionModel.add_root(instance=rootNode)
+
+        if treeString[0]['child']:
+
+            tempChild = treeString[0]['child']
+
+            for child in tempChild:
+                node = createChildObj(child, root)
+                # childObj = UserSecuritySelectionModel()
+                # childObj.classificationNameNode = ClassificationNames.objects.get(classificationName=child['classificationName'])
+                # childObj.ext_model_id= child['id']
+                # childObj.hasChildnode = child['hasChildNode']
+                # childObj.SSM=get(root.pk).SSM
+                # childObj.tgtWeight=0
+                # childObj.currWeight=0
+                root.add_child(instance=node)
+
+                if child:
+                    if child['child'] and isinstance(child['child'], dict):
+
+                        tempNode = createChildObj(child['child'], rootNode)
+
+                    elif isinstance(child['child'], list):
+
+                        tempNode = createChildObj(child['child'][0], rootNode)
+
+                    node.add_child(instance=tempNode)
+
+
+def createChildObj(child, root):
+    childObj = UserSecuritySelectionModel()
+    childObj.classificationNameNode = ClassificationNames.objects.get(classificationName=child['classificationName'])
+    childObj.ext_model_id = child['id']
+    childObj.hasChildnode = child['hasChildNode']
+    childObj.SSM = root.SSM
+    childObj.tgtWeight = 0
+    childObj.currWeight = 0
+    # add the child to the object
+
+    return childObj
