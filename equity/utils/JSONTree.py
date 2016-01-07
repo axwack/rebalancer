537c7b4adc0b8a1d574fb2f76f9759165426a393
vincentlee <axwack@gmail.com>
2016-01-07 17:09:17 -0500

    Huge milestone. tree now saves all nodes.

--- equity/utils/JSONTree.py
+++ equity/utils/JSONTree.py
@@ -1,8 +1,8 @@
 import json
+
 from equity.models import UserSecuritySelectionModel, SecuritySelectionModels, ClassificationNames
 
 get = lambda node_id: UserSecuritySelectionModel.objects.get(pk=node_id)
-
 
 def JSONtoTreeBeard(obj):
     id = None;
@@ -34,27 +34,23 @@ def JSONtoTreeBeard(obj):
             tempChild = treeString[0]['child']
 
             for child in tempChild:
+
                 node = createChildObj(child, root)
-                # childObj = UserSecuritySelectionModel()
-                # childObj.classificationNameNode = ClassificationNames.objects.get(classificationName=child['classificationName'])
-                # childObj.ext_model_id= child['id']
-                # childObj.hasChildnode = child['hasChildNode']
-                # childObj.SSM=get(root.pk).SSM
-                # childObj.tgtWeight=0
-                # childObj.currWeight=0
-                root.add_child(instance=node)
 
-                if child:
-                    if child['child'] and isinstance(child['child'], dict):
+                if not node.is_child_of(root):
+                    root.add_child(instance=node)
+                    # root.add_child(instance=node)
 
-                        tempNode = createChildObj(child['child'], rootNode)
+                    # if child:
+                    #    if child['child'] and isinstance(child['child'], dict):
 
-                    elif isinstance(child['child'], list):
+                    #       tempNode = createChildObj(child['child'], rootNode)
 
-                        tempNode = createChildObj(child['child'][0], rootNode)
+                    #   elif child['child'] and isinstance(child['child'], list):
 
-                    node.add_child(instance=tempNode)
+                    #      tempNode = createChildObj(child['child'][0], rootNode)
 
+                    # node.add_child(instance=tempNode)
 
 def createChildObj(child, root):
     childObj = UserSecuritySelectionModel()
@@ -64,6 +60,13 @@ def createChildObj(child, root):
     childObj.SSM = root.SSM
     childObj.tgtWeight = 0
     childObj.currWeight = 0
+
+    if child['hasChildNode'] == True:
+        root.add_child(instance=childObj)
+        childChildObj = createChildObj(child['child'][0], childObj)
+
+        if not childChildObj.is_child_of(childObj):
+            childObj.add_child(instance=childChildObj)
     # add the child to the object
 
     return childObj
