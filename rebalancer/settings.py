5601cc19a9f9e5410e8077d9245bc055911fe46c
vincentlee <axwack@gmail.com>
2016-02-29 20:35:31 -0500

    Added for Heroku

--- rebalancer/settings.py
+++ rebalancer/settings.py
@@ -146,12 +146,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static", "staticfiles")
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files. Added Heroku
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'equity/static'),
 )
 
 CLASSIFICATION_ROOT = 'Classification'
