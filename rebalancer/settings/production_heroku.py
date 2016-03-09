2b8f0ed9d2a3ad9bb883734605c13ccca8cf7f28
vincentlee <axwack@gmail.com>
2016-03-09 14:26:59 -0500

    Changed allowed host

--- rebalancer/settings/production_heroku.py
+++ rebalancer/settings/production_heroku.py
@@ -9,7 +9,7 @@ DATABASES['default'].update(db_from_env)
 
 # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-ALLOWED_HOSTS = ['www.portfolio270.xyz', 'herokuapp.com']
+ALLOWED_HOSTS = [*]
 
 # eMail Settings
 DEFAULT_FROM_EMAIL = 'Christopher Lee <admin@principalmvl.com>'
