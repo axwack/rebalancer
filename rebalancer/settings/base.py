5f44e880631b10d93a49cd5df9716fa862210386
vincentlee <axwack@gmail.com>
2016-03-09 15:38:13 -0500

    Changing Settings

--- rebalancer/settings/base.py
+++ rebalancer/settings/base.py
@@ -97,8 +97,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 # STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static", "staticfiles")
-STATIC_ROOT = os.path.join(BASE_DIR, "equity/static")
-STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, "equity/static")
+# STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files. Added Heroku
 # STATICFILES_DIRS = (
--- rebalancer/settings/development_settings.py
+++ rebalancer/settings/development_settings.py
@@ -5,6 +5,14 @@ TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = []
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static", "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "equity/static")
+STATIC_URL = '/static/'
+
+
+
 if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
         'default': {
