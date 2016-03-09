18be48e400264123d53124d982a20d4a225e2c66
vincentlee <axwack@gmail.com>
2016-03-09 14:13:45 -0500

    Change for path in static

--- rebalancer/settings/production_heroku.py
+++ rebalancer/settings/production_heroku.py
@@ -20,7 +20,7 @@ EMAIL_HOST_USER = 'admin@principalmvl.com'
 EMAIL_HOST_PASSWORD = '#'
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'equity/static')
-STATIC_URL = 'equity/static/'
+STATIC_URL = 'equity/static'
 
 # Extra places for collectstatic to find static files.
 # STATICFILES_DIRS = (
