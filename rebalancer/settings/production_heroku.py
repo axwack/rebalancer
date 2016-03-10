73981cf7b7a219fbc5d0403dcdd2b7a4e518ff7e
vincentlee <axwack@gmail.com>
2016-03-09 22:47:48 -0500

    Added Rebalancer to the settings.

--- rebalancer/settings/production_heroku.py
+++ rebalancer/settings/production_heroku.py
@@ -4,13 +4,13 @@ import dj_database_url
 # Added for Heroku
 # PROJECT_ROOT = os.path.dirname(__file__)  # added Heroku
 # print "Heroku [PROJECT ROOT]: %s" % (PROJECT_ROOT)
-STATIC_ROOT = os.path.join(BASE_DIR, "equity/staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "rebalancer/equity/staticfiles")
 STATIC_URL = '/static/'
 
 print "Heroku [STATIC_ROOT]: %s" % (STATIC_ROOT)
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'equity/static/'),
+    os.path.join(BASE_DIR, 'rebalancer/equity/static/'),
 )
 
 print "Heroku [STATICFILES_DIRS]: %s" % (STATICFILES_DIRS)
