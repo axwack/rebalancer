0bf0d9724a1d739012a120afdaf689d2d15cbc3d
vincentlee <axwack@gmail.com>
2016-03-11 16:28:17 -0500

    Added for admin models

--- equity/admin.py
+++ equity/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from equity.models import Security, SecuritySelectionModels, ClassificationNames, Exchanges, Countries, \
+    AllocationModels, \
+    Currencies
+
+admin.site.register(Security)
+admin.site.register(SecuritySelectionModels)
+admin.site.register(ClassificationNames)
+admin.site.register(Exchanges)
+admin.site.register(Countries)
+admin.site.register(AllocationModels)
+admin.site.register(Currencies)
