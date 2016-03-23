3f41633cb2d695e3553db389cde5211ea86bc818
vincentlee <axwack@gmail.com>
2016-03-23 14:15:37 -0400

    Changes for 1.9 Django

--- rebalancer/urls.py
+++ rebalancer/urls.py
@@ -15,62 +15,63 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+import equity.views
 
 urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^$', 'equity.views.home', name='home'),
-    url(r'^index/', 'equity.views.index', name='index'),
+    url(r'^$', equity.views.home, name='home'),
+    url(r'^index/', equity.views.index, name='index'),
 
     # url(r'^selectModelAccounts/', 'equity.views.selectModelAccounts', name='selectModelAccounts'),
 
-    url(r'^newRebalance/', 'equity.views.newRebalance', name='newRebalance'),
-    url(r'^accountSelections/', 'equity.views.accountSelections', name='accountSelections'),
+    url(r'^newRebalance/', equity.views.newRebalance, name='newRebalance'),
+    url(r'^accountSelections/', equity.views.accountSelections, name='accountSelections'),
 
-    url(r'^accounts/', 'equity.views.accounts', name='accounts'),
-    url(r'^accounts/filter/(?P<id>\d+)/$', 'equity.views.accountsByFilter', name='accountsByFilter'),
-    url(r'^filter/$', 'equity.views.accountsByFilter', name='accountsByFilter'),
+    url(r'^accounts/', equity.views.accounts, name='accounts'),
+    url(r'^accounts/filter/(?P<id>\d+)/$', equity.views.accountsByFilter, name='accountsByFilter'),
+    url(r'^filter/$', equity.views.accountsByFilter, name='accountsByFilter'),
 
-    url(r'^referenceData/', 'equity.views.referenceData', name='referenceData'),
-    url(r'^securities/', 'equity.views.securities', name='securities'),
-    url(r'^rulesCreation/', 'equity.views.rulesCreation', name='rulesCreation'),
+    url(r'^referenceData/', equity.views.referenceData, name='referenceData'),
+    url(r'^securities/', equity.views.securities, name='securities'),
+    url(r'^rulesCreation/', equity.views.rulesCreation, name='rulesCreation'),
     # url(r'^models/([0-9]+)/$', 'equity.views.updateModelWithTargetWeights', name='models'),
 
     # url(r'^updateModels/', 'equity.views.updateModelWithTargetWeights', name='updateModels'),
-    url(r'^createModelName/$', 'equity.views.Create_Model_Name', name='createModelName'),
+    url(r'^createModelName/$', equity.views.Create_Model_Name, name='createModelName'),
 
-    url(r'^accountParameters/$', 'equity.views.accountParameters', name='accountParameters'),
-    url(r'^accountSearchFilter/$', 'equity.views.accountSearchFilter', name='accountSearchFilter'),
-    url(r'^modelParameters/$', 'equity.views.modelParameters', name='modelParameters'),
-    url(r'^Account/SaveAccount/$', 'equity.views.save_account_list', name='save_account_list'),
-    url(r'^GetAllAccount/$', 'equity.views.get_account_list', name='get_account_list'),
+    url(r'^accountParameters/$', equity.views.accountParameters, name='accountParameters'),
+    url(r'^accountSearchFilter/$', equity.views.accountSearchFilter, name='accountSearchFilter'),
+    url(r'^modelParameters/$', equity.views.modelParameters, name='modelParameters'),
+    url(r'^Account/SaveAccount/$', equity.views.save_account_list, name='save_account_list'),
+    url(r'^GetAllAccount/$', equity.views.get_account_list, name='get_account_list'),
 
-    url(r'^getClassifications/$', 'equity.views.getClassifications', name='getClassifications'),
-    url(r'^getSavedClassificationModel/(?P<id>\w+)', 'equity.views.getSavedClassificationModel',
+    url(r'^getClassifications/$', equity.views.getClassifications, name='getClassifications'),
+    url(r'^getSavedClassificationModel/(?P<id>\w+)', equity.views.getSavedClassificationModel,
         name='getSavedClassificationModel'),
 
-    url(r'^saveClassifications/$', 'equity.views.saveClassifications', name='saveClassifications'),
-    url(r'^updateSecuritySelectionModels/$', 'equity.views.updateSecuritySelectionModels',
+    url(r'^saveClassifications/$', equity.views.saveClassifications, name='saveClassifications'),
+    url(r'^updateSecuritySelectionModels/$', equity.views.updateSecuritySelectionModels,
         name='updateSecuritySelectionModels'),
 
-    url(r'^deleteSecuritySelectionModels/$', 'equity.views.deleteModel', name='deleteModel'),
-    url(r'^deleteSecuritySelectionModels/(?P<id>\w+)/$', 'equity.views.deleteSecuritySelectionModels',
+    url(r'^deleteSecuritySelectionModels/$', equity.views.deleteModel, name='deleteModel'),
+    url(r'^deleteSecuritySelectionModels/(?P<id>\w+)/$', equity.views.deleteSecuritySelectionModels,
         name='deleteSecuritySelectionModels'),
 
-    url(r'^updateModelWithSecurity/(?P<id>\w+)/$', 'equity.views.updateModelWithSecurity',
+    url(r'^updateModelWithSecurity/(?P<id>\w+)/$', equity.views.updateModelWithSecurity,
         name='updateModelWithSecurity'),
-    url(r'^updateModelWithTargetWeights/$', 'equity.views.updateModelWithTargetWeights',
+    url(r'^updateModelWithTargetWeights/$', equity.views.updateModelWithTargetWeights,
         name='updateModelWithTargetWeights'),
-    url(r'^updateModelNodes/$', 'equity.views.updateModelNodes', name='updateModelNodes'),
+    url(r'^updateModelNodes/$', equity.views.updateModelNodes, name='updateModelNodes'),
 
-    url(r'^getSSMList/$', 'equity.views.get_SSMList', name='get_SSMList'),
+    url(r'^getSSMList/$', equity.views.get_SSMList, name='get_SSMList'),
 
-    url(r'^createModelWeights/$', 'equity.views.createModelWeights', name='createModelWeights'),
+    url(r'^createModelWeights/$', equity.views.createModelWeights, name='createModelWeights'),
 
-    url(r'^getModelTargetWeights/$', 'equity.views.getModelTargetWeights', name='getModelTargetWeights'),
+    url(r'^getModelTargetWeights/$', equity.views.getModelTargetWeights, name='getModelTargetWeights'),
 
-    url(r'^createSecuritySelectionModels/$', 'equity.views.createSecuritySelectionModels',
+    url(r'^createSecuritySelectionModels/$', equity.views.createSecuritySelectionModels,
         name='createSecuritySelectionModels'),
 
     # Registration
