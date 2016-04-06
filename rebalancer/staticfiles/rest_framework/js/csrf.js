79be750d80632794f404291618a62c6934f04e6f
vincentlee <axwack@gmail.com>
2016-04-05 21:12:27 -0400

    Changes to Statuc files

--- rebalancer/staticfiles/rest_framework/js/csrf.js
+++ rebalancer/staticfiles/rest_framework/js/csrf.js
@@ -29,7 +29,7 @@ function sameOrigin(url) {
     // Allow absolute or scheme relative URLs to same origin
     return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
         (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
-            // or any other URL that isn't scheme relative or absolute i.e relative.
+        // or any other URL that isn't scheme relative or absolute i.e relative.
         !(/^(\/\/|http:|https:).*/.test(url));
 }
 
