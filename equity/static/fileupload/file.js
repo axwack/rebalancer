c9fe71069a7d6aa5c92e1bf015308b00ee2ed891
vincentlee <axwack@gmail.com>
2016-05-06 12:19:33 -0400

    Javascritp for File Uploads

--- equity/static/fileupload/file.js
+++ equity/static/fileupload/file.js
@@ -16,7 +16,8 @@ var iMaxFilesize = 2621440; // 1MB
 // 500MB - 429916160
 var oTimer = 0;
 var sResultFileSize = '';
-
+var filename = "";
+var id_array;
 function secondsToTime(secs) { // we will use this function to convert seconds in normal time format
     var hr = Math.floor(secs / 3600);
     var min = Math.floor((secs - (hr * 3600)) / 60);
@@ -88,6 +89,7 @@ function fileSelected() {
         document.getElementById('filetype').innerHTML = 'Type: ' + oFile.type;
         //document.getElementById('filedim').innerHTML = 'Dimension: ' + oImage.naturalWidth + ' x ' + oImage.naturalHeight;
         //};
+        filename = oFile.name;
     };
 
     // read selected file as DataURL
@@ -110,7 +112,8 @@ function startUploading() {
     // get form data for POSTing
     //var vFD = document.getElementById('upload_form').getFormData(); // for FF3
     var vFD = new FormData(document.getElementById('upload_form'));
-    vFD.append('includeHeaders', $('.includeHeaders:checked').val());
+    vFD.append('includeHeaders', $('#includeHeaders').val());
+    vFD.append('filename', filename);
 
     // create XMLHttpRequest object, adding few event listeners, and POSTing our data
     var oXHR = new XMLHttpRequest();
@@ -118,6 +121,12 @@ function startUploading() {
     oXHR.addEventListener('load', uploadFinish, false);
     oXHR.addEventListener('error', uploadError, false);
     oXHR.addEventListener('abort', uploadAbort, false);
+
+    oXHR.onerror = function (e) {
+        if (oXHR.status == 500) {
+            console.log("Error" + e);
+        }
+    };
 
     oXHR.open('POST', ('/uploadModels/'));//Added for Django: 4/26/2016 : Vincent Lee
     oXHR.setRequestHeader("X-CSRFToken", document.getElementsByTagName('csrfmiddlewaretoken')); //Added for Django: 4/26/2016 : Vincent Lee
@@ -164,7 +173,7 @@ function uploadProgress(e) { // upload process in progress
         document.getElementById('b_transfered').innerHTML = iBytesTransfered;
         if (iPercentComplete == 100) {
             var oUploadResponse = document.getElementById('upload_response');
-            oUploadResponse.innerHTML = '<h1>Please wait...processing</h1>';
+            oUploadResponse.innerHTML = '<h3>Please wait...processing</h3>';
             oUploadResponse.style.display = 'block';
         }
     } else {
@@ -174,15 +183,17 @@ function uploadProgress(e) { // upload process in progress
 
 function uploadFinish(e) { // upload successfully finished
     var oUploadResponse = document.getElementById('upload_response');
+
     oUploadResponse.innerHTML = e.target.responseText;
     oUploadResponse.style.display = 'block';
-
     document.getElementById('progress_percent').innerHTML = '100%';
-    document.getElementById('progress').style.width = '400px';
+    document.getElementById('progress').style.width = '620px';
     document.getElementById('filesize').innerHTML = sResultFileSize;
     document.getElementById('remaining').innerHTML = '| 00:00:00';
 
     clearInterval(oTimer);
+    id_array = $.parseJSON(e.target.response);
+    $('#uploadDoneButton').prop('disabled', false);
 }
 
 function uploadError(e) { // upload error
